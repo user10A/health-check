156f888d42789cf250ecda3721cff96bef576c6c
AinazZz <124769374+Ainazzzzz@users.noreply.github.com>
2024-01-09 14:15:57 +0600

    initial commit

---
+++ health-check-java11/src/main/java/healthcheck/HealthCheckJava11Application.java
@@ -0,0 +1,13 @@
+package healthcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCheckJava11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCheckJava11Application.class, args);
+	}
+
+}
---
+++ health-check-java11/src/test/java/healthcheck/HealthCheckJava11ApplicationTests.java
@@ -0,0 +1,13 @@
+package healthcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCheckJava11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
