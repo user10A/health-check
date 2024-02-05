1fbfacd40b9cf443f97276757a8a85710f53a182
Z <peak@gmail.com>
2024-02-05 19:43:56 +0300

    Additional Methods Appointment

--- src/main/java/healthcheck/service/ApplicationService.java
+++ src/main/java/healthcheck/service/ApplicationService.java
@@ -1,5 +1,6 @@
 package healthcheck.service;
 
+import healthcheck.dto.Application.ApplicationDelete;
 import healthcheck.dto.Application.ApplicationRequest;
 import healthcheck.dto.Application.ApplicationResponse;
 import healthcheck.dto.SimpleResponse;
@@ -9,4 +10,7 @@ import java.util.List;
 public interface ApplicationService {
     SimpleResponse createApplication(ApplicationRequest applicationRequest);
     List<ApplicationResponse> getApplications(String word);
+    SimpleResponse deleteAll(List<ApplicationDelete> request);
+    SimpleResponse deleteAllPressed(ApplicationDelete request);
+    List<ApplicationResponse>getAllApplications();
 }
