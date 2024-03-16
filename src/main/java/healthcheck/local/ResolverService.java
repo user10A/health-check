b15eabc783c13f7d805d3045b7526b08b1faca6d
Erkin Toigonbaev <er@mycommit.com>
2024-03-16 17:54:22 +0600

    localization

--- src/main/java/healthcheck/local/ResolverService.java
+++
@@ -1,9 +0,0 @@
-package healthcheck.local;
-
-import healthcheck.dto.SimpleResponse;
-
-import java.util.Locale;
-
-public interface ResolverService {
-    public SimpleResponse setLocaleLanguage(Locale locale) ;
-}
