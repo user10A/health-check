1c617ed769fbd42bbb0e6f3c92e1bd783286f709
Erkin Toigonbaev <er@mycommit.com>
2024-03-19 16:57:22 +0600

    refactor

---
+++ src/main/java/healthcheck/dto/Doctor/DoctorsGetAllByDepartmentsResponse1.java
@@ -0,0 +1,12 @@
+package healthcheck.dto.Doctor;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class DoctorsGetAllByDepartmentsResponse1 {
+    private Long id;
+    private String image;
+    private String fullName;
+}
